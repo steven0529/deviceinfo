4766f459f2b2c49b9ceb67cd98ef80bfbcc143a6
Steven Reyes <sreyes@stratpoint.com>
2019-11-05 22:08:19 +0800

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+g
+7app/src/main/java/com/steven/deviceinfo/MainActivity.kt,4/c/4c66d1cd289a6063f1d7c5612b9e0c664f077295
---
+++ app/src/main/java/com/steven/deviceinfo/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.steven.deviceinfo
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_brand.text = "BRAND: ${Build.BRAND}"
+        tv_model.text = "MODEL: ${Build.MODEL}"
+        tv_manufacturer.text = "MANUFACTURER: ${Build.MANUFACTURER}"
+        tv_os_ver.text = "OS VERSION: ${Build.VERSION.RELEASE}"
+
+        val metrics = resources.displayMetrics
+
+        tv_dpi.text = "DPI: ${(metrics.density * 160f).toInt().toString()}"
+
+        val density = resources.displayMetrics.density
+
+        if (density == 0.75f) {
+            // LDPI
+            tv_density.text = "DENSITY: LDPI"
+        } else if (density >= 1.0f && density < 1.5f) {
+            // MDPI
+            tv_density.text = "DENSITY: MDPI"
+        } else if (density == 1.5f) {
+            // HDPI
+            tv_density.text = "DENSITY: HDPI"
+        } else if (density > 1.5f && density <= 2.0f) {
+            // XHDPI
+            tv_density.text = "DENSITY: XHDPI"
+        } else if (density > 2.0f && density <= 3.0f) {
+            // XXHDPI
+            tv_density.text = "DENSITY: XXHDPI"
+        } else {
+            // XXXHDPI
+            tv_density.text = "DENSITY: XXXHDPI"
+        }
+
+        tv_screen_size.text = "SIZE (WIDTH X HEIGHT): ${metrics.widthPixels} X ${metrics.heightPixels}"
+
+        val x = (metrics.widthPixels / metrics.xdpi).toDouble().pow(2.0)
+        val y = (metrics.heightPixels / metrics.ydpi).toDouble().pow(2.0)
+
+        val decimalFormat = DecimalFormat("#.##")
+
+        tv_screen_physical_size.text = "PHYSICAL SIZE: ${decimalFormat .format(sqrt(x + y))} inches"
+    }
+
+}
